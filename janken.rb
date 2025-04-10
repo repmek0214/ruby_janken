6422d64583f37ab885ca90a89e2ba843ce199c7d
repmek0214 <repmek_mk@icloud.com>
2025-04-10 15:24:27 +0900

    Update janken.rb

--- janken.rb
+++ janken.rb
@@ -2,27 +2,26 @@ puts "じゃんけん..."
 puts "0(グー)1(チョキ)2(パー)3(戦わない)"
 
 value = ["グー", "チョキ", "パー"]
-secondGameValue = ["上", "下", "右", "左"]
-statusWin = "自分の勝ち"
-statusLose = "相手の勝ち"
+second_game_value = ["上", "下", "右", "左"]
+status_win = "自分の勝ち"
+status_lose = "相手の勝ち"
 
-def play_second_game(status, secondGameValue)
+def play_second_game(status, second_game_value)
     puts "あっちむいて〜"
-    puts "0(上)1(下)2(右)3(左)"
-    
+    puts "0(上)1(下)2(右)3(左)" 
+    select_second_game_value = gets.to_i
+    random_second_game_value = second_game_value.sample
   
-    select_secondGameValue = gets.to_i
-    random_secondGameValue = secondGameValue.sample
-  
-    if (select_secondGameValue == 0) ||
-       (select_secondGameValue == 1) || 
-       (select_secondGameValue == 2) || 
-       (select_secondGameValue == 3)
+    if (select_second_game_value == 0) ||
+       (select_second_game_value == 1) || 
+       (select_second_game_value == 2) || 
+       (select_second_game_value == 3)
       puts "ホイ！"
       puts "--------"
-      puts "あなた：" + secondGameValue[select_secondGameValue]
-      puts "相手：#{random_secondGameValue}"
-      if select_secondGameValue == secondGameValue.index(random_secondGameValue)
+      puts "あなた：" + second_game_value[select_second_game_value]
+      puts "相手：#{random_second_game_value}"
+        
+      if select_second_game_value == second_game_value.index(random_second_game_value)
         puts status
       end
     else
@@ -37,7 +36,9 @@ loop do
   if select_value == 3
     puts "ゲームを終了します。"
     break
-  elsif select_value.between?(0, 2)
+  elsif (select_value == 0) || 
+        (select_value == 1) ||
+        (select_value == 2)
     puts "ホイ！"
     puts "--------"
     puts "あなたは" + value[select_value] + "を出しました。"
@@ -48,9 +49,9 @@ loop do
     elsif (select_value == 0 && value.index(random_value) == 1) || 
           (select_value == 1 && value.index(random_value) == 2) ||
           (select_value == 2 && value.index(random_value) == 0)
-      play_second_game(statusWin, secondGameValue)
+      play_second_game(status_win, second_game_value)
     else
-      play_second_game(statusLose, secondGameValue)
+      play_second_game(status_lose, second_game_value)
     end
   else
     puts "無効な入力です。もう一度試してください。"
